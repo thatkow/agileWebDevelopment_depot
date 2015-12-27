72f916e38fd1ce1eb87cbaa81ac6ceb0a68a7e51
Andrew Kowalczyk <kow.andrew@gmail.com>
2015-12-28 00:25:37 +1100

    db migration. Combining multiple entries into single with quantity value
    rails generate migration combine_items_in_cart
    rake db:migrate
    rake db:rollback
    rake db:migrate
    rake db:migrate:status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227131151) do
+ActiveRecord::Schema.define(version: 20151227131731) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
