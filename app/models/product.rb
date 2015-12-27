449c5e56a9826547fd075d66b8370299123d2b58
Andrew Kowalczyk <kow.andrew@gmail.com>
2015-12-27 22:35:32 +1100

    Validation - Fields are present - Price is positive numeric - Titles are unique - Image formats are valid - Created tests to reflect these validations - Create fixture
    rake test
    rake test:models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true # Ensures every element has an entry
+	validates :price, numericality: {greater_than_or_equal_to: 0.01} # Positive price only
+	validates :title, uniqueness: true # Is unique in database
+	validates :image_url, allow_blank: true, format: { # Control formats
+		with:
+		%r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
