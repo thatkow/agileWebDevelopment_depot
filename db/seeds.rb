4bbc55e06ca5fc2fc0b80ef494f98ceacd6fcdbe
Andrew Kowalczyk <kow.andrew@gmail.com>
2015-12-27 21:56:18 +1100

    Added a scaffold, some test database entries, initial migration, change routing
    rails generate scaffold Product title:string description:text image_url:string price:decimal
    rake db:migrate
    rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'ruby.jpg',
+price: 49.95)
+Product.create!(title: 'Andrew\'s Book',
+description:
+%{<p>
+This is a book all about Andrew Kowalczyk.
+</p>},
+image_url: 'andrew.jpg',
+price: 19.95)
